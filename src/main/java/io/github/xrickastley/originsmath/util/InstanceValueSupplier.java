fb24f4e115f02dc21b1218849a9794335cbae219
xrickastley <reodiqueridge10@gmail.com>
2025-07-01 17:59:22 +0800

    Added default implementation for supplyValue

--- src/main/java/io/github/xrickastley/originsmath/util/InstanceValueSupplier.java
+++ src/main/java/io/github/xrickastley/originsmath/util/InstanceValueSupplier.java
@@ -1,6 +1,11 @@
 package io.github.xrickastley.originsmath.util;
 
 public interface InstanceValueSupplier<T> {
-	public int supplyValue(T instance);
 	public Number supplyAsNumber(T instance);
+	
+	default int supplyValue(T instance) {
+		return this
+			.supplyAsNumber(instance)
+			.intValue();
+	}
 }
